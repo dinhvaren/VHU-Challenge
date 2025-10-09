8f64f95f8d3d88b692a068180a5caebee691ecd1
dinhvaren <hqhuyzzzz@gmail.com>
2025-10-09 10:30:52 +0700

    change html to hbs

--- src/app/controller/HomeController.js
+++ src/app/controller/HomeController.js
@@ -1,6 +1,6 @@
 class HomeController {
   index(req, res, next) {
-    res.render("admin/leaderboard", { page: { title: "VHU InfoSec Lab" } });
+    res.render("admin/teams", { page: { title: "VHU InfoSec Lab" } });
   }
 }
 
