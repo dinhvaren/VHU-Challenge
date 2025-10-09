9899f2be70382a40be488c5bb1600ef2f6115375
dinhvaren <hqhuyzzzz@gmail.com>
2025-10-09 09:29:55 +0700

    update model

--- src/app/models/Challenge.js
+++ src/app/models/Challenge.js
@@ -6,8 +6,8 @@ const challengeSchema = new mongoose.Schema({
   points:      { type: Number, required: true },
   difficulty:  { type: String, enum: ["Easy", "Medium", "Hard"], default: "Easy" },
   description: { type: String, required: true },
-  attachments: [{ type: String }], // link đến file hoặc resource
-  flag:        { type: String, required: true }, // flag thật, được hash SHA256 trong môi trường production
+  attachments: [{ type: String }],
+  flag:        { type: String, required: true }, 
   status:      { type: String, enum: ["Active", "Draft"], default: "Active" },
   createdAt:   { type: Date, default: Date.now }
 });
